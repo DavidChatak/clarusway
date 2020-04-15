32ebcfd0c6c1ee1dbb5e10cfade6a1ed35cd8368
David Chatak <davidchatak40@gmail.com>
2020-04-15 22:17:16 +0300

    2. 15042020 commit

---
+++ Assignment_String.py
@@ -0,0 +1,13 @@
+import string
+
+kucukharfler = string.ascii_lowercase
+
+print(*kucukharfler)
+
+
+
+BÜYÜK_harfler = string.ascii_uppercase
+
+print(*BÜYÜK_harfler)
+print(string.octdigits)
+print(*string.printable)
