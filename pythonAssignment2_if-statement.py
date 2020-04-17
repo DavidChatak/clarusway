32cd43fd633d6b558e4470792e8a5e8cba20b6d3
DavidChatak <63158138+DavidChatak@users.noreply.github.com>
2020-04-17 21:34:10 +0300

    Update pythonAssignment2_if-statement.py

--- pythonAssignment2_if-statement.py
+++ pythonAssignment2_if-statement.py
@@ -1,3 +1,5 @@
+#pythonAssignment2_if-statement.py
+
 age = input("Are you a cigarette addict older than 75 years old? (Yes/No)>>")
 chronic = input("Do you have a severe chronic disease? (Yes/No)>>")
 immune = input("Is your immune system too weak? (Yes/No)>>")
